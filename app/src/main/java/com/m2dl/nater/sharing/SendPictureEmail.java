2efa0c1b7875266fb6a9fc45c686cdc40b26c940
IljaKroonen <ilja.kroonen@gmail.com>
2015-01-15 11:30:29 +0100

    Email works if no attachment, test needed for attachments

--- app/src/main/java/com/m2dl/nater/sharing/SendPictureEmail.java
+++ app/src/main/java/com/m2dl/nater/sharing/SendPictureEmail.java
@@ -6,17 +6,27 @@ import com.m2dl.nater.data.Picture;
 
 import com.m2dl.nater.utils.GMailSender;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SendPictureEmail implements ISendPicture {
+    final GMailSender sender = new GMailSender(SenderProvider.APPLICATION_EMAIL, SenderProvider.APPLICATION_PASSWORD);
+
     @Override
-    public void sendPicture(Picture picture) {
-        GMailSender sender = new GMailSender(SenderProvider.APPLICATION_EMAIL, SenderProvider.APPLICATION_PASSWORD);
-
-        try {
-            sender.addAttachment(picture.mImageUri.getPath(), "pic");
-            sender.sendMail("Nater image upload", picture.toString(), SenderProvider.APPLICATION_EMAIL, SenderProvider.SHARING_EMAIL);
-        } catch (Exception e) {
-            Log.e("SendMail", e.getMessage(), e);
-        }
-
+    public void sendPicture(final Picture picture) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (sender) {
+                        sender.addAttachment(picture.mImageUri.getPath(), "pic");
+                        sender.sendMail("Nater image upload", picture.toString(), SenderProvider.APPLICATION_EMAIL, SenderProvider.SHARING_EMAIL);
+                    }
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+            }
+        });
+        t.start();
     }
 }
