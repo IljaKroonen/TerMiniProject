6ec9f6786ce4fe0c6d31b8cfe9e9461c25210857
IljaKroonen <ilja.kroonen@gmail.com>
2015-01-15 09:17:14 +0100

    More work on sharing

---
+++ app/src/main/java/sharing/SendPictureEmail.java
@@ -0,0 +1,10 @@
+package sharing;
+
+import android.graphics.Picture;
+
+public class SendPictureEmail implements ISendPicture {
+    @Override
+    public void sendPicture(Picture picture) {
+
+    }
+}
